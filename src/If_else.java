ea7771ef4f8a359c5ae2d5ca15afaead7abc85cf
Ciuulin <danielaciutulin@gmail.com>
2018-06-17 11:56:09 +0100

    My homework

---
+++ src/If_else.java
@@ -0,0 +1,9 @@
+public class If_else {
+    public static void main(String args []){
+        int age=20;
+        if (!(age>15&& age>17)){
+            System.out.println("da");
+        }
+        System.out.println("The end");
+    }
+}
